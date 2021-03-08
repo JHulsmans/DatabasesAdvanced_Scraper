8faf3717e07dcf1ef055eac2f4f41860a909aaa7
Senne <ennes.w@pm.me>
2021-03-08 14:52:40 +0100

    upd + redis to mongo docker

--- redis-to-mongo.py
+++ redis-to-mongo.py
@@ -1,4 +1,20 @@
 import json
 import pymongo as mongo
+import redis
 
 #redis push the highest value to mongodb
+def toMongo():
+    r = redis.Redis('localhost')
+    mydata = r.get('btc-data')
+
+    data = json.loads(mydata)
+
+    client = mongo.MongoClient("mongodb://127.0.0.1:27017")
+    btc_data_db = client["btc-data"]
+    col_btc = btc_data_db["data_btc"]
+    x = col_btc.insert_one(data[0])
+
+#Run every minute
+while(True):
+    toMongo()
+    time.sleep(60)
